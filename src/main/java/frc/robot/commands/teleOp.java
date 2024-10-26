234bb16d724cbe28aaaa50c4dca06c9cfd4b6d7f
alan <alanthomasat123@gmail.com>
2024-10-26 13:36:11 -0700

    finalizedcodetooeasy

--- src/main/java/frc/robot/commands/teleOp.java
+++ src/main/java/frc/robot/commands/teleOp.java
@@ -5,37 +5,38 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
 
-/** An example command that uses an example subsystem. 
- * @param <ExampleSubsystem>*/
-public class teleOp<ExampleSubsystem> extends Command {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
+public class TeleOp extends Command {
   Joystick joystick;
+  Drivetrain dt;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public teleOP(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+  public TeleOp(Drivetrain dt) {
+
+    addRequirements(dt);
   }
 
-  // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
+  public void initialize() {
+    dt.tankDrive(0, 0);
+  }
   @Override
-  public void execute() {}
+  public void execute() {
+    final double leftPowerRaw = joystick.getRawAxis(1);
+    final double rightPowerRaw = joystick.getRawAxis(5);
+    dt.tankDrive(leftPowerRaw, rightPowerRaw);
+    SmartDashboard.putNumber("leftPower", leftPowerRaw);
+    SmartDashboard.putNumber("rightPower", rightPowerRaw);
+  }
 
-  // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    dt.tankDrive(0,0);
 
+  }
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -18,8 +18,8 @@ public class Drivetrain extends SubsystemBase {
 
  
   public Drivetrain() {
-    leftDriveTalon = new WPI_TalonSRX(Constants.DrivetrainPorts.leftDriveTalonPort);
-    rightDriveTalon = new WPI_TalonSRX(Constants.DrivetrainPorts.leftDriveTalonPort);
+    leftDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.leftDriveTalonPort);
+    rightDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.leftDriveTalonPort);
     leftDriveTalon.setNeutralMode(NeutralMode.Coast);
     rightDriveTalon.setNeutralMode(NeutralMode.Coast);
     rightDriveTalon.setInverted(true);
@@ -32,7 +32,7 @@ public class Drivetrain extends SubsystemBase {
     rightDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
   }
 
- public void TankDrive(double leftSpeed, double rightSpeed) {
+ public void tankDrive(double leftSpeed, double rightSpeed) {
   leftDriveTalon.set(leftSpeed);
   rightDriveTalon.set(rightSpeed);
  }
@@ -45,28 +45,12 @@ public class Drivetrain extends SubsystemBase {
   navx.reset();
  }
 
- public void getTicks() {
-  private int leftPosition = leftDriveTalon.getSelectedSensorPosition(0);
-  private int rightPosition = rightDriveTalon.getSelectedSensorPosition(0);
-  private int avgPosition = (leftPosition + rightPosition)/2;
-}
-
-public void getMeters() {
-  private double metersPerTick = (0.1524 * Math.PI)/4096;
-  private double positionInMeters = avgPosition * metersPerTick;
-}
-
-public void resetEncoders() {
-  leftDriveTalon.getSelectedSensorPosition(0,0,10);
-  rightDriveTalon.getSelectedSensorPosition(0,0,10);
-}
-
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    SmartDashboard.putNumber(key:"Left Voltage", leftDriveTalon.getMotorOutputPercent());
-    SmartDashboard.putNumber(key:"Right Voltage", rightDriveTalon.getMotorOutputPercent());
-    SmartDashboard.putNumber(key: "Angle", navx.getAngle());
+    SmartDashboard.putNumber("Left Voltage", leftDriveTalon.getMotorOutputPercent());
+    SmartDashboard.putNumber("Right Voltage", rightDriveTalon.getMotorOutputPercent());
+    SmartDashboard.putNumber("Angle", navx.getAngle());
   }
 
   @Override
